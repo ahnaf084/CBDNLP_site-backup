814fb9db1a25a9545319a9939b91e570b9a6dc2c
ahnaf084 <ahnaf084@gmail.com>
2021-11-03 01:06:29 +0600

    New

--- app.py
+++ app.py
@@ -1,51 +1,32 @@
-import pickle
-
-import numpy as np
+import ktrain
 import streamlit as st
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-category = ['Geopolitical', 'Hate speech', 'Personal attack', 'Political', 'Profanity', 'Religious', 'Sexual harassment']
+category = ['Geopolitical', 'Hate speech', 'Personal attack', 'Political', 'Profanity', 'Religious',
+            'Sexual harassment']
 
 
 def predict(comm, catg):
-    model = load_model(
-        "CyberbullyingDetection.h5")
-    # model.summary()
+    model = ktrain.load_predictor('CyberbullyingDetection_kt')
 
-    with open('tokenizer.pickle', 'rb') as handle:
-        tokenizer = pickle.load(handle)
+    cat = model.predict(comm)
+    probas = model.predict_proba(comm)
 
-    sentence = [comm]
-    sequences = tokenizer.texts_to_sequences(sentence)
-    padded = pad_sequences(sequences, maxlen=1334, padding='post', truncating='post')
-    prediction_main = model.predict(padded)
-
-    i = 0
-    for k in range(len(prediction_main[0])):
-        print(category[i] + ' = %f %%' % (prediction_main[0, k] * 100))
-        i = i + 1
-
-    result = (np.where(prediction_main == max(prediction_main[0])))
-
-    catg = result[1][0]
-
-    return max(prediction_main[0]), catg
+    return cat, round(max(probas) * 100, 2)
 
 
-catgr = 0
+percentage = 0
 
-st.title("Cyberbullying Detection using NLP")
-comment = st.text_area("Enter a comment in Bangla text")
+st.title("Cyberbullying Detection")
+comment = st.text_area("Enter text in Bangla")
 
-if st.button("Analyze"):
+if st.button("Detect"):
     with st.spinner("Analyzing the text …"):
-        prediction = predict(comment, catgr)
-        if prediction[0] > 0.8:
-            st.success("This comment contains {:.2%} ".format(prediction[0]) + category[prediction[1]])
+        prediction = predict(comment, percentage)
+        if prediction[1] > 80:
+            st.success("This comment contains " + str(prediction[1]) + "% " + str(prediction[0]))
             # st.balloons()
-        elif 0.6 < prediction[0] < 0.8:
-            st.error("This might be a cyberbullying comment which contains {:.0%} ".format(prediction[0]) + category[
-                prediction[1]])
+        elif 60 < prediction[1] < 80:
+            st.error("This might be a cyberbullying comment which contains " + str(prediction[1]) + "% " + str(
+                prediction[0]))
         else:
             st.warning("This is not a cyberbullying comment")
